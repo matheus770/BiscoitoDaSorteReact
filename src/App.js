5eea97c99345d5d5d2f34933c622f35a5f1a6598
NecoMatheus <matheus770@users.noreply.github.com>
2023-01-05 21:32:06 -0300

    Biscoito da sorte React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './estilo.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {};
+        this.quebraBiscoito = this.quebraBiscoito.bind(this);
+
+        this.frases = ['A vida trará coisas boas se tiver paciência.', 'Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.', 'Defeitos e virtudes são apenas dois lados da mesma moeda.'];
+
+    }
+
+    quebraBiscoito(){
+        let state = this.state;
+        let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+        state.textoFrase ='" ' +this.frases[numeroAleatorio]+ ' "';
+        this.setState(state);
+    }
+
+    render(){
+        return(
+            <div className='container'>
+                <img src={require('./assets/biscoito-da-sorte.png')} className='img'/>
+                <Botao nome='AbrirBiscoito' acaoBtn={this.quebraBiscoito}/>
+                <h3 className='textofrase'>{this.state.textoFrase}</h3>
+            </div>
+        );
+    }
+}
+
+class Botao extends Component{
+    render(){
+        return(
+            <div>
+                <button onClick={this.props.acaoBtn}>{this.props.nome}</button>
+            </div>
+        );
+    }
 }
 
 export default App;
